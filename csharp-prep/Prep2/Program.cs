b0607209c0888012f936a92132a6a4c0d8a17329
Kayla Lange <k_lange@live.com>
2024-09-16 12:22:40 -0600

    added code to request grade percentage from user, determine letter grade and sign and print grade and statement telling user if they passed or not

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string gradePercentage = Console.ReadLine();
+
+        int percent = int.Parse(gradePercentage);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >=70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int dividend = percent;
+        int divisor = 10;
+        int remainder = dividend % divisor;
+
+        string sign = "";
+
+        if (remainder >= 7 && letter != "A" && letter != "F")
+        {
+            sign = "+";
+        }
+        else if (remainder < 3 && letter != "F")
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{sign}.");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. Let's look at your options!");
+        }
+
     }
+
 }

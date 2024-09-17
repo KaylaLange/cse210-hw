c0481d06383c597b1549911b2319983ae7e565a9
Kayla Lange <k_lange@live.com>
2024-09-17 11:05:23 -0600

    created a number guessing game using a random generator. used a do while loop to continue to loop through the game if the user types yes. created a while loop to prompt the user to keep guessing until they get it right and included clues to help them get to the right number.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber;
+        int userGuess;
+        string response;
+
+        do
+        {
+            magicNumber = randomGenerator.Next(1, 101);
+            userGuess = -1;
+            response = "";
+            int guessCount = 0;
+
+            while (userGuess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                userGuess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                    Console.WriteLine("");
+                    Console.Write("Do you want to play again? ");
+                    response = Console.ReadLine();
+                }
+            }
+        } while (response == "yes");
+
     }
 }

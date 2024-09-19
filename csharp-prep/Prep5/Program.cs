44f6153500e93a29f2caf74e802ee67d10a05fb7
Kayla Lange <k_lange@live.com>
2024-09-18 22:29:15 -0600

    programmed functions to provide output and request input and utilize input to calculate the square of the number

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int faveNumber = PromptUserNumber();
+        int squared = SquareNumber(faveNumber);
+        DisplayResult(name, squared);
     }
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int faveNumber = int.Parse(Console.ReadLine());
+        return faveNumber;
+    }
+
+    static int SquareNumber(int faveNumber)
+    {
+        int squared = faveNumber * faveNumber;
+        return squared;
+    }
+
+    static void DisplayResult(string name, int squared)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squared}");
+    }    
 }
